316bb3b9f2a835238f2df7cfe02b076109590175
BihanMadhusankha <ellawalagemadhusankha02@gmail.com>
2024-09-10 02:05:25 +0530

    update to resonsive design

--- Server/server.js
+++ Server/server.js
@@ -1,21 +1,31 @@
 const baseUrl = "https://api.weatherapi.com/v1/forecast.json?key=";
 const apiKey = "b8390d3353264932983185912240509";
-document.addEventListener("DOMContentLoaded", () => {
-    const searchBar = document.getElementById("Search_bar");
-    const searchBtn = document.getElementById("Search_btn");
+
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success);
+} else {
+    console.log("Geolocation is not supported by this browser.");
+}
+
+// User Current Location
+function success(position) {
+
     const currentWeatherImage = document.getElementById('currentWeatherImage');
     const currentLocation = document.getElementById('currentLocation');
     const currentTempreture = document.getElementById('currentTempreture');
     const currentWeather = document.getElementById('currentWeather');
 
-    searchBtn.addEventListener("click", async (event) => {
-        event.preventDefault();
-        
-        try {
-            const searchVal = searchBar.value;
-            const response = await fetch(`${baseUrl}${apiKey}&q=${searchVal}&days=7`);
-            const data = await response.json();
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
 
+    console.log("Latitude: " + latitude);
+    console.log("Longitude: " + longitude);
+
+    fetch(`${baseUrl}${apiKey}&q=${latitude},${longitude}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
             if (data.current) {
                 currentWeatherImage.src = `${data.current.condition.icon} `;
                 currentLocation.innerHTML = `${data.location.name} `;
@@ -24,10 +34,109 @@ document.addEventListener("DOMContentLoaded", () => {
             } else {
                 currentWeatherImage.src = "No data found";
             }
+        })
+        .catch(error => console.log('Error fetching weather data: ', error));
+
+}
+
+// digital clock
+function updateClock() {
+    const today = new Date();
+    let h = today.getHours();
+    const meridiem = h >= 12 ? 'PM' : 'AM';
+    h = h % 12 || 12;
+    h = h.toString().padStart(2, '0');
+    const m = today.getMinutes().toString().padStart(2, '0');
+    const s = today.getSeconds().toString().padStart(2, '0');
+    const timeString = `${h}: ${m}: ${s} ${meridiem}`;
+
+    document.getElementById('clock').textContent = timeString;
+    var t = setTimeout(updateClock, 500);
+};
+updateClock();
+setInterval(updateClock, 1000);
+
+
+//Search location
+document.addEventListener("DOMContentLoaded", () => {
+    const searchBar = document.getElementById("Search_bar");
+    const searchBtn = document.getElementById("Search_btn");
+    const searchLocation = document.getElementById('searchLocation');
+    const searchTemprature = document.getElementById('searchTemprature');
+    const searchHumidity = document.getElementById('searchHumidity');
+    const searchWindSpeed = document.getElementById('searchWindSpeed');
+    const searchCondition = document.getElementById('searchCondition');
+    const searchRegion = document.getElementById('searchRegion');
+    const searchCountry = document.getElementById('searchCountry');
+    const searchLongitude = document.getElementById('searchLongitude');
+    const searchLatitude = document.getElementById('searchLatitude');
+
+    searchBtn.addEventListener("click", async (event) => {
+        event.preventDefault();
+
+        try {
+            const searchVal = searchBar.value;
+            const response = await fetch(`${baseUrl}${apiKey}&q=${searchVal}&days=7`);
+            const data = await response.json();
+
+            if (data.current) {
+
+                searchLocation.innerHTML = `${data.location.name} `;
+                searchTemprature.innerHTML = `${data.current.temp_c} °C`;
+                searchHumidity.innerHTML = `${data.current.humidity} `;
+                searchWindSpeed.innerHTML = `${data.current.wind_kph} `;
+                searchCondition.innerHTML = `${data.current.condition.text}`;
+                searchRegion.innerHTML = `${data.location.region} `;
+                searchCountry.innerHTML = `${data.location.country} `;
+                searchLongitude.innerHTML = `${data.location.lon}`;
+                searchLatitude.innerHTML = `${data.location.lat} `;
+            } else {
+                currentWeatherImage.src = "No data found";
+            }
         } catch (error) {
             alert("Error fetching weather data: ", error);
         }
     });
 });
+
+// weather forcasting
+document.addEventListener("DOMContentLoaded", () => {
+    
+        const searchBtn = document.getElementById("Search_btn");
+        const searchVal = document.getElementById("Search_bar").value;
+    let reop = {
+        method: 'POST'
+    };
+    fetch(`${baseUrl}${apiKey}&q=${searchVal}&days=7`, reop)
+        .then(response => response.json())
+        .then(data => {
+            const startDate = new Date(`${data.forecast.forecastday[0].date}`);
+            let currentDay = new Date(startDate);
+            
+
+            searchBtn.addEventListener("click", async (event) => {
+                event.preventDefault();
+                for (let i = 0; i < 7; i++) {
+                    //hethuw blann toISOString, split
+                    const formattedDate = currentDay.toISOString().split('T')[0];
+
+                    fetch(`${baseUrl}${apiKey}&q=${searchVal}&days=7&dt=${formattedDate}&aqi=homagama&alerts=yes`)
+                        .then(response => response.json())
+                        .then(data => {
+
+                            document.getElementById(`weekDays${i + 1}`).innerHTML = `${data.forecast.forecastday[0].date}`
+                            document.getElementById(`imageWeather${i + 1}`).src = `${data.forecast.forecastday[0].day.condition.icon}`
+                            document.getElementById(`temp${i + 1}`).innerHTML = `${data.forecast.forecastday[0].day.maxtemp_c} °C`
+
+                        })
+                        .catch(error => {
+                            console.error("Error:", error);
+                        });
+
+                    currentDay.setDate(currentDay.getDate() + 1);
+                }
+            })
+        });
+    });
 
 // °C
